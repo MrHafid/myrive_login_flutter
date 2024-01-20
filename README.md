251f26eee4b0e9a505c27b0fefe777018312377b
Mr Hafid <achmadhafid668@gmail.com>
2024-01-20 09:51:50 +0700

    after edited

---
+++ README.md
@@ -0,0 +1,18 @@
+# Rive animation in flutter
+
+https://github.com/davfru/flutter-custom-login/assets/16302609/2b8a6229-a5fb-4803-97f3-ef3ddcfd986e
+
+Install an emulator (android or ios), then to run the app:
+
+```bash
+flutter pub get
+flutter run
+```
+
+credits: https://www.youtube.com/watch?v=Td3xEWwRAQA&t=23s
+
+
+
+
+
+
---
+++ lib/screens/onboding/onboding_screen.dart
@@ -0,0 +1,106 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:rive_animation/screens/onboding/components/animated_btn.dart';
+import 'package:rive_animation/screens/onboding/components/custom_sign_in.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  bool isSignInDialogShown = false;
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation("active", autoplay: false);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Positioned(
+            width: MediaQuery.of(context).size.width * 1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset('assets/Backgrounds/Spline.png')),
+        Positioned.fill(
+            child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+        )),
+        const RiveAnimation.asset('assets/RiveAssets/shapes.riv'),
+        Positioned.fill(
+            child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 20, sigmaY: 10),
+          child: const SizedBox(),
+        )),
+        AnimatedPositioned(
+          duration: Duration(milliseconds: 240),
+          top: isSignInDialogShown ? -50 : 0,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Spacer(),
+                    const SizedBox(
+                      width: 260,
+                      child: Column(children: [
+                        Text(
+                          "Learn design & code",
+                          style: TextStyle(
+                              fontSize: 60, fontFamily: "Poppins", height: 1.2),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                            "Don't skip design. Learn design and code, by builder real apps with Flutter and Swift. Complete courses about best tools.")
+                      ]),
+                    ),
+                    const Spacer(
+                      flex: 2,
+                    ),
+                    AnimatedBtn(
+                      btnAnimationController: _btnAnimationController,
+                      press: () {
+                        _btnAnimationController.isActive = true;
+                        Future.delayed(Duration(milliseconds: 800), () {
+                          setState(() {
+                            isSignInDialogShown = true;
+                          });
+                          customSigninDialog(context, onClosed: (_) {
+                            setState(() {
+                              isSignInDialogShown = false;
+                            });
+                          });
+                        });
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24.0),
+                      child: Text(
+                        "Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates",
+                        style: TextStyle(),
+                      ),
+                    )
+                  ]),
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}
